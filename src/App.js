6389079ccef9c1b340ce598ddf79f5b8581c6e5e
Vijay Kumar <vijay.kumar@emvigotech.com>
2021-08-10 22:15:52 +0530

    Fixed Header Position

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './components/navBar/NavBar'
 import Article from './components/articles/Article'
 import './App.css'
-
+import { makeStyles } from '@material-ui/core'
+const useStyles = makeStyles(theme => ({
+  offset: theme.mixins.toolbar,
+}))
 function App() {
+  const classes = useStyles()
+
   return (
     <>
       <Router>
         <Switch>
           <div>
             <NavBar />
+            <div className={classes.offset} />
             <Route path='/' exact component={Article} />
           </div>
         </Switch>
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles, useTheme, useMediaQuery } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Drawer from "../Drawer"
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import { makeStyles, useTheme, useMediaQuery } from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Drawer from '../Drawer'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
-        overflow: "hidden"
+        overflow: 'hidden',
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -20,53 +21,51 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
     },
     logo: {
-        width: "17vw"
-    }
-}));
+        width: '17vw',
+    },
+}))
 
 export default function ButtonAppBar() {
     const theme = useTheme()
     const isMatch = useMediaQuery(theme.breakpoints.down('sm'))
 
-    const classes = useStyles();
-    const [show, setShow] = useState(false);
+    const classes = useStyles()
+    const [show, setShow] = useState(false)
 
     useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
 
     const handleScroll = () => {
-        setShow(window.pageYOffset > 140);
-    };
+        setShow(window.pageYOffset > 140)
+    }
 
     return (
         <>
-            {
-                isMatch ? (<Drawer />) : (
-
-                    <div className={classes.root}>
-                        <AppBar position="static">
-                            <Toolbar>
-                                {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton> */}
-                                <Typography variant="h6" className={classes.title}>
-                                    <img className={classes.logo} src="https://cdn-static.spcafe.in/articles/white-logo-asset.svg" alt="logo" />
-                                </Typography>
-                                <Typography variant="h6" className={classes.title}>
-                                    News Feeds
-                        </Typography>
-
-                                <Button color="inherit" onClick={() => window.scrollTo(0, 0)}>About Us</Button>
-
-
-                            </Toolbar>
-                        </AppBar>
-                    </div>
-
-                )
-            }
+            {isMatch ? (
+                <Drawer />
+            ) : (
+                <div className={classes.root}>
+                    <AppBar position='fixed'>
+                        <Toolbar>
+                            <Typography variant='h6' className={classes.title}>
+                                <img
+                                    className={classes.logo}
+                                    src='https://cdn-static.spcafe.in/articles/white-logo-asset.svg'
+                                    alt='logo'
+                                />
+                            </Typography>
+                            <Typography variant='h6' className={classes.title}>
+                                News Feeds
+                            </Typography>
+                            <Button color='inherit' onClick={() => window.scrollTo(0, 0)}>
+                                About Us
+                            </Button>
+                        </Toolbar>
+                    </AppBar>
+                </div>
+            )}
         </>
-    );
+    )
 }
